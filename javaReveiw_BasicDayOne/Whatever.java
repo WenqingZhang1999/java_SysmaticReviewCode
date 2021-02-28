48881dd1e332cf9cb164094392f3ad3cd6ded1ea
WenqingZhang1999 <74153505+WenqingZhang1999@users.noreply.github.com>
2021-02-28 02:34:46 +0000

    Add files via upload
    Basic_DayOne

---
+++ javaReveiw_BasicDayOne/Whatever.java
@@ -0,0 +1,11 @@
+
+public class Whatever {
+    static long lala;
+
+    public static void main(String[] args) {
+        int a = 0x64;
+        System.out.println("最大值：Byte.MAX_VALUE=" + Byte.MAX_VALUE);
+        System.out.println("Bool :" + lala);
+        System.out.println(a);
+    }
+}
